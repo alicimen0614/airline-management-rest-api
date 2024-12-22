453a1d004a6eceeb6aac04aa391f6c99c179d149
Ali Çimen <alicimen0614@gmail.com>
2024-12-22 15:53:14 +0300

    Added endpoints to fetch flights of a specific airplane and reservations of a specific flight

--- flights/views.py
+++ flights/views.py
@@ -1,8 +1,11 @@
+from django.shortcuts import get_object_or_404
+
 from flights import serializers
 from flights import models
 
 from rest_framework import viewsets
 from rest_framework.decorators import action
+from rest_framework.response import Response
 
 
 class AirplaneViewSet(viewsets.ModelViewSet):
@@ -10,13 +13,31 @@ class AirplaneViewSet(viewsets.ModelViewSet):
     serializer_class = serializers.AirplaneSerializer
     queryset = models.Airplane.objects.all()
 
+    @action(detail=True, methods=['GET'])
+    def flights(self, request, pk=None):
+        """Custom action to retrieve flights of a specific airplane"""
+        airplane = get_object_or_404(models.Airplane, pk=pk)
+        flights = airplane.flights.all()
+        serializer = serializers.FlightSerializer(flights, many=True)
+
+        return Response(serializer.data)
+
 
 class FlightViewSet(viewsets.ModelViewSet):
     """Handle creating, reading and updating flights"""
     serializer_class = serializers.FlightSerializer
     queryset = models.Flight.objects.all()
 
+    @action(detail=True, methods=['GET'])
+    def reservations(self, request, pk=None):
+        """Custom action to retrieve reservations of a specific flight"""
+        flight = get_object_or_404(models.Flight, pk=pk)
+        reservations = flight.reservations.all()
+        serializer = serializers.ReservationSerializer(reservations, many=True)
+
+        return Response(serializer.data)
     
+
 class ReservationViewSet(viewsets.ModelViewSet):
     """Handle creating, reading and updating reservations"""
     serializer_class = serializers.ReservationSerializer
